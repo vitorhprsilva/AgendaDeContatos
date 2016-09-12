f63d3af4633a26235ddb314a24ad10cdb26328a2
Fernando Salles Claro <fsclaro@gmail.com>
2016-09-12 15:08:52 -0300

    Primeiro Commit

---
+++ src/in/alphadev/view/AgendaDeContatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.alphadev.view;
+
+/**
+ *
+ * @author fernando
+ */
+public class AgendaDeContatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaAgenda tela = new TelaAgenda();
+        tela.setVisible(true);
+    }
+    
+}
